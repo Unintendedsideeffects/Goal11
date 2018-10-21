a602eb6ac4dcb8469370e9a8c54f2ccaafdd03f6
f3drc <federicolippolis94@gmail.com>
2018-10-21 07:22:03 +0200

    Extract conflict fixed

--- ExtractConflicts.py
+++ ExtractConflicts.py
@@ -1,8 +1,12 @@
 import csv
 import cmath
 import datetime
+import distance
 
-def Extract(pathFile, coordinatesPoint, distanceMax, yearsAgo):
+
+pathFile = "ConflictData.csv"
+
+def extract(coordinatesPoint, distanceMax, yearsAgo):
         
     with open(pathFile) as csv_file:
 
@@ -10,28 +14,22 @@ def Extract(pathFile, coordinatesPoint, distanceMax, yearsAgo):
 
         coordinates = []
         validRows = []
-        R = 6372.795477598
         now = datetime.datetime.now()
 
         for row in csv_reader:
             coordinates = [float(row["LATITUDE"]),float(row["LONGITUDE"])]
             year = int(row["YEAR"])
 
-            latA = (coordinatesPoint[0]*cmath.pi)/180
-            lonA = (coordinatesPoint[1]*cmath.pi)/180
-
-            latB = (coordinates[0]*cmath.pi)/180
-            lonB = (coordinates[1]*cmath.pi)/180
-
-            distance = R*cmath.acos(cmath.sin(latA)*cmath.sin(latB) + cmath.cos(latA)*cmath.cos(latB)*cmath.cos(lonA-cmath.cos(lonB)))
-
-            if float(distance.real) <=distanceMax and now.year - year < yearsAgo:
+            if distance.distance(coordinatesPoint,coordinates,distanceMax) and now.year - year < yearsAgo:
                 validRows.append(row)
 
     return validRows
 
+def numberConflicts(coordinatesPoint, distanceMax, yearsAgo):
+    return len(extract(coordinatesPoint, distanceMax, yearsAgo))
+
 # print ("START")
-# events=Extract(pathFile = "ConflictData.csv",coordinatesPoint = [32.1736,36.1940],distanceMax=100,yearsAgo=10)
+#print(numberConflicts(coordinatesPoint = [32.1736,36.1940],distanceMax=150,yearsAgo=10))
 # for event in events:
 #     print("------------")
 #     print(event)

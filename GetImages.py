45b9cd89455ecef0e9d4600a7a4bb624e9abace6
Unknown <snake2895@gmail.com>
2018-10-21 02:47:35 +0200

    Update getImage method

--- GetImages.py
+++ GetImages.py
@@ -1,40 +1,36 @@
-import datetime
 import numpy as np
-
-import matplotlib.pyplot as plt
-
-from sentinelhub import WmsRequest, WcsRequest, MimeType, CRS, BBox
+import datetime
+from sentinelhub import WmsRequest, CRS, BBox
+from PIL import Image
+import os
+import sys 
 
 INSTANCE_ID = "301131b2-14c0-4797-b938-9b48914ae592" 
+IMAGE_TYPE = ["TRUE-COLOR-S2-L1C", "NDWI", "NDVI"]
+PATH_TYPE = ["Main", "Water", "Vegetation"]
 
-def plot_image(image, factor=1):
-    """
-    Utility function for plotting RGB images.
-    """
-    fig = plt.subplots(nrows=1, ncols=1, figsize=(15, 7))
+def downloadImage(name,  coords, type = 0, date = "latest", width = 1920):
 
-    if np.issubdtype(image.dtype, np.floating):
-        plt.imshow(np.minimum(image * factor, 1))
-    else:
-        plt.imshow(image)
+    #TODO get coord by name
 
+    bbox = BBox(bbox=coords, crs=CRS.WGS84)
 
-coords = [36.541, 31.917, 36.613, 31.887]
-bbox = BBox(bbox=coords, crs=CRS.WGS84)
+    true_color_request = WmsRequest(layer=IMAGE_TYPE[type],
+                                        bbox=bbox,
+                                        time=date, 
+                                        width=width,
+                                        maxcc=0.2,
+                                        instance_id=INSTANCE_ID)
 
-true_color_request = WmsRequest(layer='TRUE-COLOR-S2-L1C',
-                                    bbox=bbox,
-                                    time='latest',
-                                    height=1080,
-                                    instance_id=INSTANCE_ID)
+    wms_true_color_img = true_color_request.get_data()
 
-wms_true_color_img = true_color_request.get_data()
-#print (wms_true_color_img)
-#plot_image(wms_true_color_img)
-print('Returned data is of type = %s and length %d.' % (type(wms_true_color_img), len(wms_true_color_img)))
-print('Single element in the list is of type {} and has shape {}'.format(type(wms_true_color_img[-1]), wms_true_color_img[-1].shape))
-#plt.imshow(wms_true_color_img[-1], interpolation='nearest');
+    im = Image.fromarray(wms_true_color_img[-1])
+    path =  "./" + str(name) + "/" + PATH_TYPE[type] 
+    if (not os.path.isdir(path)):    
+        os.makedirs(path)
+    path = path + "/" + str(date) + ".png" 
+    im.save(path, 'PNG')
+    return path
 
-from PIL import Image
-im = Image.fromarray(wms_true_color_img[-1])
-im.save("now.png")
+#test
+downloadImage("NOME_SETTLEMENT",coords = [36.541, 31.917, 36.613, 31.887])

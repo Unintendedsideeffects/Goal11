203f293f7d28fd0b12a9dbda3050e6e50399ece5
f3drc <federicolippolis94@gmail.com>
2018-10-21 06:29:42 +0200

    Extract settlements script

--- ExtractSettlements.py
+++ ExtractSettlements.py
@@ -1,7 +1,9 @@
 import csv
 import distance
 
-def extractSettlements (pathFile, coordinatesPoint, radius):
+pathFile = "Settlements.csv"
+
+def extractSettlements (coordinatesPoint, radius):
     with open(pathFile) as csv_file:
 
         csv_reader = csv.DictReader(csv_file, delimiter = ",")
@@ -15,8 +17,30 @@ def extractSettlements (pathFile, coordinatesPoint, radius):
                 validRows.append(row)
 
     return validRows
-                
-def getSettlementsByName(pathFile, name):
+
+def getNearest(coordinatesPoint):
+    nearest = None
+    minDist = -1
+
+    with open(pathFile) as csv_file:
+
+        csv_reader = csv.DictReader(csv_file, delimiter = ",")
+
+        coordinates = []
+
+        for row in csv_reader:
+            coordinates = [float(row["LAT"]),float(row["LONG"])]
+            dist = distance.distance(coordinatesPoint,coordinates)
+            if minDist==-1:
+                minDist = dist
+                nearest = row
+            elif dist<minDist:
+                minDist= dist
+                nearest = row
+
+    return nearest
+
+def getSettlementsByName(name):
     with open(pathFile) as csv_file:
 
         csv_reader = csv.DictReader(csv_file, delimiter = ",")
@@ -25,13 +49,13 @@ def getSettlementsByName(pathFile, name):
             if row["NAME"] == name:
                 return row
 
-def getSettlementsByPcode(pathFile, Pcode):
+def getSettlementsByPcode(Pcode):
     with open(pathFile) as csv_file:
 
         csv_reader = csv.DictReader(csv_file, delimiter = ",")
 
         for row in csv_reader:
-            if row["NAME"] == name:
+            if row["ï»¿PCode"] == Pcode:
                 return row
 
 
@@ -39,4 +63,6 @@ def getSettlementsByPcode(pathFile, Pcode):
 #rows = extractSettlements("Settlements.csv", [-4.764447,13.614718],50)
 #print(rows)
 
-getSettlementsByName("Settlements.csv","Madimba")
+#getSettlementsByName("Madimba")
+#getSettlementsByPcode("CG010100015")
+#getNearest([-4.764447,13.614718])
